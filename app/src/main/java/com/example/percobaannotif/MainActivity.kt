bf805d137bc2d4918cab56a3334e4653eb93c2f7
Varatlan <148843870+Varatlan@users.noreply.github.com>
2024-05-08 10:11:10 +0700

    tesr

---
+++ app/src/main/java/com/example/percobaannotif/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.percobaannotif
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    //hello
+    //kokokok
+    //llololololo
+    //awokawokawok
+}
